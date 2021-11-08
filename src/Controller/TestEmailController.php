4dfc4dc644a3238f845de8222a473555668318b5
Yaroslav <yaroslav.k@zimalab.com>
2021-11-09 04:05:13 +0700

    adding egg fix

--- src/Controller/TestEmailController.php
+++ src/Controller/TestEmailController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Mailer\Mailer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestEmailController extends AbstractController
 {
